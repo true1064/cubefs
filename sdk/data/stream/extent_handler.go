6782a7a01da77c0dada2f36a183ae536f8c8a3e2
chihe <chi.he@oppo.com>
2023-11-04 23:52:25 -0700

    feature(client): [hybrid cloud] supports the option to choose SSD or HDD data partition
    Signed-off-by: chihe <chi.he@oppo.com>

--- sdk/data/stream/extent_handler.go
+++ sdk/data/stream/extent_handler.go
@@ -114,11 +114,13 @@ type ExtentHandler struct {
 	lastKey   proto.ExtentKey
 
 	meetLimitedIoError bool
+
+	mediaType uint32
 }
 
 // NewExtentHandler returns a new extent handler.
-func NewExtentHandler(stream *Streamer, offset int, storeMode int, size int) *ExtentHandler {
-	// log.LogDebugf("NewExtentHandler stack(%v)", string(debug.Stack()))
+func NewExtentHandler(stream *Streamer, offset int, storeMode int, size int, storageClass uint32) *ExtentHandler {
+	//	log.LogDebugf("NewExtentHandler stack(%v)", string(debug.Stack()))
 	eh := &ExtentHandler{
 		stream:             stream,
 		id:                 GetExtentHandlerID(),
@@ -132,6 +134,8 @@ func NewExtentHandler(stream *Streamer, offset int, storeMode int, size int) *Ex
 		doneSender:         make(chan struct{}),
 		doneReceiver:       make(chan struct{}),
 		meetLimitedIoError: false,
+		verUpdate:          make(chan uint64),
+		mediaType:          storageClass,
 	}
 
 	go eh.receiver()
@@ -589,7 +593,7 @@ func (eh *ExtentHandler) recoverPacket(packet *Packet) error {
 		// Always use normal extent store mode for recovery.
 		// Because tiny extent files are limited, tiny store
 		// failures might due to lack of tiny extent file.
-		handler = NewExtentHandler(eh.stream, int(packet.KernelOffset), proto.NormalExtentType, 0)
+		handler = NewExtentHandler(eh.stream, int(packet.KernelOffset), proto.NormalExtentType, 0, eh.mediaType)
 		handler.setClosed()
 	}
 	handler.pushToRequest(packet)
@@ -621,7 +625,7 @@ func (eh *ExtentHandler) allocateExtent() (err error) {
 
 	for i := 0; i < MaxSelectDataPartitionForWrite; i++ {
 		if eh.key == nil {
-			if dp, err = eh.stream.client.dataWrapper.GetDataPartitionForWrite(exclude); err != nil {
+			if dp, err = eh.stream.client.dataWrapper.GetDataPartitionForWrite(exclude, eh.mediaType); err != nil {
 				log.LogWarnf("allocateExtent: failed to get write data partition, eh(%v) exclude(%v), clear exclude and try again!", eh, exclude)
 				exclude = make(map[string]struct{})
 				continue
