4a1952450dcd76980b60337e669d2e07a5ac6c9d
chihe <chi.he@oppo.com>
2023-11-12 20:41:52 -0800

    feature(metanode,client):[hybrid cloud] support hybridcloud data migration
    Signed-off-by: chihe <chi.he@oppo.com>

--- master/cluster.go
+++ master/cluster.go
@@ -3581,7 +3581,7 @@ func (c *Cluster) createVol(req *createVolReq) (vol *Vol, err error) {
 			req.name, readWriteDataPartitions, proto.MediaTypeString(chosenMediaType))
 	}
 
-	vol.dataPartitions.updateResponseCache(true, 0, vol.VolType)
+	vol.dataPartitions.updateResponseCache(true, 0, vol)
 	vol.updateViewCache(c)
 	// NOTE: update dp view cache
 	vol.dataPartitions.updateResponseCache(true, 0, vol)
