82d0099fc8176338889c7a67182e4401b677cc98
chihe <chi.he@oppo.com>
2023-12-20 00:59:05 -0800

    enhance(metanode): [hybrid cloud]1.Simplify the logic of updating extent key after migration 2. reset inode reserved when excute marshall operation 3. initial ebs client in mp 4. remove cold vol or hot vol check in client and objectnode
    Signed-off-by: chihe <chi.he@oppo.com>

--- client/fs/super.go
+++ client/fs/super.go
@@ -247,7 +247,7 @@ func NewSuper(opt *proto.MountOptions) (s *Super, err error) {
 		MinWriteAbleDataPartitionCnt: opt.MinWriteAbleDataPartitionCnt,
 		OnRenewalForbiddenMigration:  s.mw.RenewalForbiddenMigration,
 		CacheDpStorageClass:          s.cacheDpStorageClass,
-		VolStorageClass:              opt.VolStorageClass,
+		AllowedStorageClass:          opt.AllowedStorageClass,
 	}
 
 	s.ec, err = stream.NewExtentClient(extentConfig)
@@ -256,7 +256,7 @@ func NewSuper(opt *proto.MountOptions) (s *Super, err error) {
 	}
 	s.mw.VerReadSeq = s.ec.GetReadVer()
 
-	if proto.IsStorageClassBlobStore(opt.VolStorageClass) {
+	if proto.VolSupportsBlobStore(opt.AllowedStorageClass) {
 		s.ebsc, err = blobstore.NewEbsClient(access.Config{
 			ConnMode: access.NoLimitConnMode,
 			Consul: access.ConsulConfig{
@@ -282,7 +282,7 @@ func NewSuper(opt *proto.MountOptions) (s *Super, err error) {
 	}
 
 	s.suspendCh = make(chan interface{})
-	if proto.IsStorageClassBlobStore(opt.VolStorageClass) {
+	if proto.VolSupportsBlobStore(opt.AllowedStorageClass) {
 		go s.scheduleFlush()
 	}
 	if s.mw.EnableSummary {
