0e00aead76ea045228202320c1bac3c340d450a7
true1064 <tangjingyu@oppo.com>
2024-01-02 19:31:57 +0800

    feat(master): [hybrid cloud] handle compatibility of master when older version upgrade to hybrid cloud: (1) auto set mediaType of datanode, zone, datapartiton by config legacy "legacyDataMediaType" and persist; (2) auto set storageClass of volume by config legacy "legacyDataMediaType" and persist; (3) if need to rollback master to older version, need to do nothing, just replace to old master and restart. because hybrid cloud master only add new fields to meta by not changed old fields. Signed-off-by: true1064 <tangjingyu@oppo.com>

--- proto/admin_proto.go
+++ proto/admin_proto.go
@@ -1316,10 +1316,6 @@ func MediaTypeString(mediaType uint32) (value string) {
 	return
 }
 
-// const ForbiddenMigrationRenewalPeriod = 2 * time.Minute
-// TODO:chihe debug
-const ForbiddenMigrationRenewalPeriod = 10 * time.Second
-
 func IsValidMediaType(mediaType uint32) bool {
 	if mediaType >= MediaType_SSD && mediaType <= MediaType_HDD {
 		return true
@@ -1416,3 +1412,7 @@ func GetStorageClassByMediaType(mediaType uint32) (storageClass uint32) {
 
 	return
 }
+
+// const ForbiddenMigrationRenewalPeriod = 2 * time.Minute
+// TODO:chihe debug
+const ForbiddenMigrationRenewalPeriod = 10 * time.Second
