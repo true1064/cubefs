3dbaf72550ba71bba03a4a4701315f3bbe917fb7
true1064 <tangjingyu@oppo.com>
2024-01-10 20:16:08 +0800

    feat(meta): [hybrid cloud] handle compatibility when old version client send createInode request without field StorageClass #21960067 Signed-off-by: true1064 <tangjingyu@oppo.com>

--- proto/admin_proto.go
+++ proto/admin_proto.go
@@ -1124,8 +1124,8 @@ type SimpleVolView struct {
 	DpRepairBlockSize uint64
 
 	// hybrid cloud
-	AllowedStorageClass []uint32
 	VolStorageClass     uint32
+	AllowedStorageClass []uint32
 	CacheDpStorageClass uint32
 }
 
