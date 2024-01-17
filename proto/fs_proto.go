261ef1011f5285f54619493c732b9c41e364618b
chihe <chihe@oppo.com>
2024-01-17 10:38:03 +0800

    feature(metanode):add audit log for migration
    Signed-off-by: chihe <chihe@oppo.com>

--- proto/fs_proto.go
+++ proto/fs_proto.go
@@ -1055,6 +1055,7 @@ type UpdateExtentKeyAfterMigrationRequest struct {
 	StorageClass     uint32         `json:"storageClass"`
 	NewObjExtentKeys []ObjExtentKey `json:"newObjExtentKeys"`
 	WriteGen         uint64         `json:"writeGen"`
+	RequestExtend
 }
 
 type InodeGetWithEkResponse struct {
