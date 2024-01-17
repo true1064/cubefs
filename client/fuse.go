86f4d1252023f5806af98806b02ae4cf472bf461
true1064 <tangjingyu@oppo.com>
2024-01-17 20:39:46 +0800

    fix(client): [hybrid cloud] Fix some places where the original condition "vol allowedStorage supports blobstore" should be changed to "vol storageClass is blobstore". Adds field volStorageClass to ExtentClient for this purpose. Remove field volumeType from ExtentClient by the way.
    Signed-off-by: true1064 <tangjingyu@oppo.com>

--- client/fuse.go
+++ client/fuse.go
@@ -407,7 +407,7 @@ func main() {
 	}
 
 	proto.InitBufferPool(opt.BuffersTotalLimit)
-	if proto.VolSupportsBlobStore(opt.AllowedStorageClass) {
+	if proto.VolSupportsBlobStore(opt.VolAllowedStorageClass) {
 		buf.InitCachePool(opt.EbsBlockSize)
 	}
 	if opt.EnableBcache {
@@ -743,7 +743,7 @@ func mount(opt *proto.MountOptions) (fsConn *fuse.Conn, super *cfs.Super, err er
 				super.CacheAction = volumeInfo.CacheAction
 				super.CacheThreshold = volumeInfo.CacheThreshold
 				super.EbsBlockSize = volumeInfo.ObjBlockSize
-			} else if proto.VolSupportsBlobStore(opt.AllowedStorageClass) {
+			} else if proto.VolSupportsBlobStore(opt.VolAllowedStorageClass) {
 				super.EbsBlockSize = volumeInfo.ObjBlockSize
 			}
 		}
@@ -988,9 +988,9 @@ func loadConfFromMaster(opt *proto.MountOptions) (err error) {
 	opt.TxTimeout = volumeInfo.TxTimeout
 	opt.TxConflictRetryNum = volumeInfo.TxConflictRetryNum
 	opt.TxConflictRetryInterval = volumeInfo.TxConflictRetryInterval
-	opt.CacheDpStorageClass = volumeInfo.CacheDpStorageClass
-	opt.AllowedStorageClass = volumeInfo.AllowedStorageClass
 	opt.VolStorageClass = volumeInfo.VolStorageClass
+	opt.VolAllowedStorageClass = volumeInfo.AllowedStorageClass
+	opt.VolCacheDpStorageClass = volumeInfo.CacheDpStorageClass
 
 	var clusterInfo *proto.ClusterInfo
 	clusterInfo, err = mc.AdminAPI().GetClusterInfo()
