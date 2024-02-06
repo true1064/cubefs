4d20d7eb57ddae10a830900e3efc3969bb4856f3
chihe <chihe@oppo.com>
2024-02-06 09:10:31 +0800

    feature(metanode):Delaying deletion of data before migration
    Signed-off-by: chihe <chihe@oppo.com>

--- metanode/partition_fsm.go
+++ metanode/partition_fsm.go
@@ -469,6 +469,8 @@ func (mp *metaPartition) Apply(command []byte, index uint64) (resp interface{}, 
 			return
 		}
 		err = mp.fsmSetCreateTime(req)
+	case opFSMInternalFreeInodeMigrationExtentKey:
+		err = mp.internalDeleteMigrationExtentKey(msg.V)
 	default:
 		// do nothing
 	}
