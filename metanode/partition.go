e1bb7845068fce11a771bf811a2e1e05c9ff9353
true1064 <tangjingyu@oppo.com>
2024-05-17 19:28:12 +0800

    fix(meta): [hybrid cloud] when create root inode, set its storageClass as the volume's VolstorageClass #22196488 #22201235 Signed-off-by: tangjingyu <tangjingyu@oppo.com>

--- metanode/partition.go
+++ metanode/partition.go
@@ -528,6 +528,7 @@ type metaPartition struct {
 	enableAuditLog         bool
 	statByStorageClass     []*proto.StatOftorageClass
 	fmList                 *forbiddenMigrationList
+	volStorageClass        uint32
 }
 
 func (mp *metaPartition) IsForbidden() bool {
@@ -761,6 +762,7 @@ func (mp *metaPartition) onStart(isCreate bool) (err error) {
 	go mp.runVersionOp()
 
 	mp.volType = volumeInfo.VolType
+	mp.volStorageClass = volumeInfo.VolStorageClass
 
 	if clusterInfo.EbsAddr != "" && proto.IsVolSupportStorageClass(volumeInfo.AllowedStorageClass, proto.StorageClass_BlobStore) {
 		var ebsClient *blobstore.BlobStoreClient
