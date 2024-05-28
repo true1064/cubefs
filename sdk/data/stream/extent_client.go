c836d6402bff149852003784ebe07e08084a6dd9
true1064 <tangjingyu@oppo.com>
2024-05-28 19:44:40 +0800

    fix(sdk): [hybrid cloud] set VolCacheDpStorageClass when invoking NewExtentClient #22196488 Signed-off-by: tangjingyu <tangjingyu@oppo.com>

--- sdk/data/stream/extent_client.go
+++ sdk/data/stream/extent_client.go
@@ -308,6 +308,7 @@ retry:
 	client.preload = config.Preload
 	client.disableMetaCache = config.DisableMetaCache
 	client.renewalForbiddenMigration = config.OnRenewalForbiddenMigration
+	client.CacheDpStorageClass = config.VolCacheDpStorageClass
 
 	var readLimit, writeLimit rate.Limit
 	if config.ReadRate <= 0 {
@@ -340,8 +341,6 @@ retry:
 
 	log.LogInfof("max streamer limit %d", client.maxStreamerLimit)
 	client.streamerList = list.New()
-
-	client.CacheDpStorageClass = config.VolCacheDpStorageClass
 
 	go client.backgroundEvictStream()
 
