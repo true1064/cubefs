9049094d732f2f73f071eff3e0899c3e05c508d1
true1064 <tangjingyu@oppo.com>
2024-05-28 15:16:20 +0800

    refactor(master): [hybrid cloud] refactor the check of createVolReq Signed-off-by: tangjingyu <tangjingyu@oppo.com>

--- master/api_service.go
+++ master/api_service.go
@@ -2683,6 +2683,13 @@ func (m *Server) checkCreateVolReq(req *createVolReq) (err error) {
 	}
 
 	req.coldArgs = args
+
+	if proto.IsHot(req.volType) && (req.dpReplicaNum == 1 || req.dpReplicaNum == 2) && !req.followerRead {
+		err = fmt.Errorf("hot volume dpReplicaNum(%v) less than 3, followerRead must set true", req.dpReplicaNum)
+		log.LogErrorf("[checkCreateVolReq] creating vol(%v) err:%v", req.name, err.Error())
+		return err
+	}
+
 	return nil
 }
 
@@ -2703,12 +2710,6 @@ func (m *Server) createVol(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err = m.checkCreateVolReq(req); err != nil {
-		sendErrReply(w, r, &proto.HTTPReply{Code: proto.ErrCodeParamError, Msg: err.Error()})
-		return
-	}
-
-	if proto.IsHot(req.volType) && (req.dpReplicaNum == 1 || req.dpReplicaNum == 2) && !req.followerRead {
-		err = fmt.Errorf("hot volume replicaNum be 2 and 3,followerRead must set true")
 		sendErrReply(w, r, &proto.HTTPReply{Code: proto.ErrCodeParamError, Msg: err.Error()})
 		return
 	}
